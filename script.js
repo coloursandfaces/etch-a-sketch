b4547e4c8640dd01d687592129464bd90d74a8e0
coloursandfaces <victorouyang98@gmail.com>
2023-03-07 13:31:19 -0800

    Created function for grid and initial hover event

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(16, 16);
+
+//const box = document.getElementById('box');
+container.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = "black";
+})
